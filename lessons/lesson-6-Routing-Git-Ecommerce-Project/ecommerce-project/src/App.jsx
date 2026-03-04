c2c95c372a9fc9ea441566cbcd0882cb891fad8b
roto1001 <roberttomor8@gmail.com>
2026-03-04 10:38:32 +0100

    Added Orders Page and Tracking Page to ecommerce project.

--- lessons/lesson-6-Routing-Git-Ecommerce-Project/ecommerce-project/src/App.jsx
+++ lessons/lesson-6-Routing-Git-Ecommerce-Project/ecommerce-project/src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router'
 import { HomePage } from "./pages/HomePage.jsx";
 import { CheckoutPage } from "./pages/CheckoutPage.jsx";
+import { OrdersPage } from "./pages/OrdersPage.jsx";
+import { TrackingPage } from "./pages/TrackingPage.jsx";
 import './App.css'
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
             <Routes>
                 <Route index element={<HomePage />} />
                 <Route path="checkout" element={<CheckoutPage />} />
+                <Route path="orders" element={<OrdersPage />} />
+                <Route path="tracking" element={<TrackingPage />} />
             </Routes>
         </>
   )
