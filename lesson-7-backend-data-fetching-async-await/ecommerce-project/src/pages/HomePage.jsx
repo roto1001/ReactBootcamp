80abf64c10ba8b19200331258e7c41251155df03
roto1001 <roberttomor8@gmail.com>
2026-03-05 12:45:16 +0100

    Added fetch data from the backend for lesson 7.

--- lesson-7-backend-data-fetching-async-await/ecommerce-project/src/pages/HomePage.jsx
+++ lesson-7-backend-data-fetching-async-await/ecommerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,13 @@ import { Header } from '../components/Header';
 import {products} from "../../starting-code/data/products.js";
 
 export function HomePage() {
+    fetch('http://localhost:3000/api/products')
+        .then((response) => {
+            return response.json()
+        }).then((data) => {
+            console.log(data);
+        });
+
     return (
         <>
             <link rel="icon" type="image/svg+xml+png" href="/home-favicon.png" />
