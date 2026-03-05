d08a27967d6a8dc8b9ae16552fa1268e3962eaa3
roto1001 <roberttomor8@gmail.com>
2026-03-05 13:05:58 +0100

    Add server proxy config for /api and /images

--- lesson-7-backend-data-fetching-async-await/ecommerce-project/vite.config.js
+++ lesson-7-backend-data-fetching-async-await/ecommerce-project/vite.config.js
@@ -5,6 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000'
+      },
+      '/images': {
+        target: 'http://localhost:3000'
+      }
+    },
     watch: {
       usePolling: true,
     },
